a71b9c47c54c491d7815b4a1dbb775b18d6b8bed
Rael.lazar <nelsongdelossantos14@gmail.com>
2025-03-29 20:19:29 +0100

    Vista de administrador añadida junto con el crud y el nav del crud

---
+++ app/views/UltimateCrud.php
@@ -0,0 +1,147 @@
+<?php
+$orderType = $_GET["order"] ?? '';
+switch ($orderType) {
+    case 'usu': ?>
+        <h2 class="text-xl font-bold mb-4 text-custom-red">Lista de Usuarios</h2>
+        <table class="w-full border-collapse">
+            <thead class="border-t border-custom-red">
+                <tr class="text-gray-700">
+                    <th class="px-4 py-2 text-left">ID</th>
+                    <th class="px-4 py-2 text-left">Nombre</th>
+                    <th class="px-4 py-2 text-left">Contraseña</th>
+                    <th class="px-4 py-2 text-left">Apellidos</th>
+                    <th class="px-4 py-2 text-left">Email</th>
+                    <th class="px-4 py-2 text-left">Dirección</th>
+                    <th class="px-4 py-2 text-left">Rol</th>
+                    <th class="px-4 py-2 text-left">Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($users as $usu): ?>
+                    <tr>
+                        <td class="px-4 py-2"><?= $usu->id_user; ?></td>
+                        <td class="px-4 py-2"><?= $usu->first_name; ?></td>
+                        <td class="px-4 py-2 break-all max-w-xs overflow-x-auto "><?= $usu->password; ?></td>
+                        <td class="px-4 py-2"><?= $usu->last_name; ?></td>
+                        <td class="px-4 py-2"><?= $usu->email; ?></td>
+                        <td class="px-4 py-2"><?= $usu->address; ?></td>
+                        <td class="px-4 py-2"><?= $usu->role; ?></td>
+                        <td class="px-4 py-2">
+                            <div class="flex space-x-2">
+                                <a href="index.php?order=delete&id=<?= $usu->id_user ?>" class="text-red-500 hover:text-red-900 transition duration-300">
+                                    <i class="fas fa-trash"></i>
+                                </a>
+                                <a href="index.php?order=mod&id=<?= $usu->id_user ?>" class="text-yellow-500 hover:text-yellow-900 transition duration-300">
+                                    <i class="fas fa-edit"></i>
+                                </a>
+                            </div>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    <?php break;
+
+    case 'order': ?>
+        <h2 class="text-xl font-bold mb-4 text-custom-red">Lista de Pedidos</h2>
+        <table class="w-full border-collapse">
+            <thead class="border-t border-custom-red">
+                <tr class="text-gray-700">
+                    <th class="px-4 py-2 text-left">ID</th>
+                    <th class="px-4 py-2 text-left">Fecha</th>
+                    <th class="px-4 py-2 text-left">Precio Total</th>
+                    <th class="px-4 py-2 text-left">Dirección</th>
+                    <th class="px-4 py-2 text-left">Status</th>
+                    <th class="px-4 py-2 text-left">Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($orders as $ord): ?>
+                    <tr>
+                        <td class="px-4 py-2"><?= $ord->id_order; ?></td>
+                        <td class="px-4 py-2"><?= $ord->order_date; ?></td>
+                        <td class="px-4 py-2"><?= $ord->total_price; ?></td>
+                        <td class="px-4 py-2"><?= $ord->delivery_address; ?></td>
+                        <td class="px-4 py-2"><?= $ord->status; ?></td>
+                        <td class="px-4 py-2">
+                            <div class="flex space-x-2">
+                                <a href="index.php?order=details&id=<?= $ord->id_order ?>" class="text-blue-500 hover:text-blue-900 transition duration-300">
+                                    <i class="fas fa-info-circle"></i>
+                                </a>
+                                <a href="index.php?order=delete&id=<?= $ord->id_order ?>" class="text-red-500 hover:text-red-900 transition duration-300">
+                                    <i class="fas fa-trash"></i>
+                                </a>
+                                <a href="index.php?order=mod&id=<?= $ord->id_order ?>" class="text-yellow-500 hover:text-yellow-900 transition duration-300">
+                                    <i class="fas fa-edit"></i>
+                                </a>
+                            </div>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    <?php break;
+
+    case 'dish': ?>
+        <h2 class="text-xl font-bold mb-4 text-custom-red">Lista de Platos</h2>
+        <table class="w-full border-collapse">
+            <thead class="border-t border-custom-red">
+                <tr class="text-gray-700">
+                    <th class="px-4 py-2 text-left">ID</th>
+                    <th class="px-4 py-2 text-left">Nombre</th>
+                    <th class="px-4 py-2 text-left">Precio</th>
+                    <th class="px-4 py-2 text-left">Tipo</th>
+                    <th class="px-4 py-2 text-left">Detalles</th>
+                    <th class="px-4 py-2 text-left">Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($dishes as $dish): ?>
+                    <tr>
+                        <td class="px-4 py-2"><?= $dish->id_dish; ?></td>
+                        <td class="px-4 py-2"><?= $dish->name; ?></td>
+                        <td class="px-4 py-2"><?= $dish->price; ?></td>
+                        <td class="px-4 py-2"><?= $dish->type; ?></td>
+                        <td class="px-4 py-2"><?= $dish->details; ?></td>
+                        <td class="px-4 py-2">
+                            <div class="flex space-x-2">
+                                <a href="index.php?order=details&id=<?= $dish->id_dish ?>" class="text-blue-500 hover:text-blue-900 transition duration-300">
+                                    <i class="fas fa-info-circle"></i>
+                                </a>
+                                <a href="index.php?order=delete&id=<?= $dish->id_dish ?>" class="text-red-500 hover:text-red-900 transition duration-300">
+                                    <i class="fas fa-trash"></i>
+                                </a>
+                                <a href="index.php?order=mod&id=<?= $dish->id_dish ?>" class="text-yellow-500 hover:text-yellow-900 transition duration-300">
+                                    <i class="fas fa-edit"></i>
+                                </a>
+                            </div>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+<?php break;
+}
+
+?>
+<div class="flex justify-center mt-6">
+    <form method="get" action="index.php" class="flex space-x-2 ">
+        <input type="hidden" name="order" value="<?= $_GET['order'] ?? '' ?>">
+        <button type="submit" name="nav" value="Begin" 
+            class="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700 transition duration-300">
+            << 
+        </button>
+        <button type="submit" name="nav" value="Before" 
+            class="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700 transition duration-300">
+            < 
+        </button>
+        <button type="submit" name="nav" value="Next" 
+            class="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700 transition duration-300">
+            > 
+        </button>
+        <button type="submit" name="nav" value="Last" 
+            class="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700 transition duration-300">
+            >> 
+        </button>
+    </form>
+</div>
--- index.php
+++ index.php
@@ -1,7 +1,103 @@
 <?php
-
+session_start();
 include_once 'app/controllers/DishController.php';
 include_once 'app/helpers/utilities.php';
-
+include_once 'app\models\DatabaseConnection.php';
+include_once 'app\models\Dish.php';
+include_once 'app\models\User.php';
+include_once 'app\models\Order.php';
+include_once 'app\models\Allergen.php';
+include_once 'app\controllers\Actions.php';
+ob_start();
 $controller = new DishController();
 $controller->showRandomDishes();
+define('FPAG', 5);
+$db = DatabaseConnection::getModel();
+$usufiles = $db->getNumUsers();
+
+if ($usufiles % FPAG == 0) {
+    $posfin = $usufiles - FPAG;
+} else {
+    $posend = $usufiles - ($usufiles % FPAG);
+}
+
+if (!isset($_SESSION['posini'])) {
+    $_SESSION['posini'] = 0;
+}
+$posAux = $_SESSION['posini'];
+
+if (isset($_GET['order'])) {
+    if (isset($_SESSION['last_order']) && $_SESSION['last_order'] !== $_GET['order']) {
+        $_SESSION['posini'] = 0;
+        $posAux = 0;
+    }
+    $_SESSION['last_order'] = $_GET['order'];
+}
+
+if ($_SERVER['REQUEST_METHOD'] == "GET") {
+
+//controles para el crudnav
+    if (isset($_GET['nav'])) {
+        switch ($_GET['nav']) {
+            case "Begin":
+                $posAux = 0;
+                break;
+            case "Next":
+                $posAux += FPAG;
+                if ($posAux > $posfin) $posAux = $posfin;
+                break;
+            case "Before":
+                $posAux -= FPAG;
+                if ($posAux < 0) $posAux = 0;
+                break;
+            case "Last":
+                $posAux = $posfin;
+        }
+        $_SESSION['posini'] = $posAux;
+    }
+
+
+    //controles para las ordenes
+    if (isset($_GET["order"])) {
+        $order = $_GET["order"];
+    } else {
+        $order = null;
+    }
+
+    switch ($order) {
+        case 'main':
+            $controller->showRandomDishes();
+            break;
+
+        case 'admin':
+            ob_clean();
+            require_once 'app\views\adminview.php';
+            break;
+        case 'usu':
+            ob_clean();
+            
+            $posini = $_SESSION['posini'];
+            $users = $db->getUsers(FPAG, $posini);
+            include_once 'app\views\adminview.php';
+            break;
+
+        case 'order':
+            ob_clean();
+            $posini = $_SESSION['posini'];
+            $orders = $db->getOrders(FPAG, $posini);
+            include_once 'app\views\adminview.php';
+            break;
+
+        case 'dish':
+            ob_clean();
+            $posini = $_SESSION['posini'];
+            $dishes = $db->getDishes(FPAG, $posini);
+            include_once 'app\views\adminview.php';
+            break;
+
+        case 'delete':
+            ob_clean();
+            DeleteCrud($_GET["id"], $_GET["type"]);
+            break;
+    }
+}
